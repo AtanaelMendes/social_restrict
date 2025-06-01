9b908d7dc283d08c3e22a160e32c9a7ca15c1eea
Atanael Mendes <atanaelmendes@gmail.com>
2025-05-31 23:53:26 -0300

    crinaod tutorial ios

--- lib/background_main.dart
+++ lib/background_main.dart
@@ -1,11 +1,10 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_screentime/modules/home/apps_controller.dart';
 import 'package:flutter_screentime/android/widgets/ask_permission_dialog.dart';
 import 'package:flutter_screentime/navigation_service.dart';
 import 'package:flutter_screentime/modules/qr-code/qrviewpage.dart';
 import 'package:flutter_screentime/politica_de_privacidade.dart';
-import 'package:flutter_screentime/tutorial_page.dart';
+import 'package:flutter_screentime/tutorial_android_page.dart';
 import 'package:get/get.dart';
 
 class BackgroundMain extends GetView {
@@ -39,93 +38,92 @@ class BackgroundMainPage extends GetView<AppsController> {
         title: Text(title),
       ),
       body: SafeArea(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    // 1. Logo do App
-                    Column(
-                      children: [
-                        Image.asset(
-                          'assets/icon/180.png',
-                          width: 120,
-                          height: 120,
-                        ),
-                        const SizedBox(height: 20),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              // 1. Logo do App
+              Column(
+                children: [
+                  Image.asset(
+                    'assets/icon/180.png',
+                    width: 120,
+                    height: 120,
+                  ),
+                  const SizedBox(height: 20),
 
-                        // 2. Texto explicativo
-                        const Text(
-                          "Solicite o QR Code ao seu guardião para escanear e utilizar o app, mas antes clique em \"VER TUTORIAL\"",
-                          textAlign: TextAlign.center,
-                          style: TextStyle(fontSize: 20),
-                        ),
-                      ],
-                    ),
-
-                    const SizedBox(height: 10),
-
-                    // 3. Botão LER QR CODE
-                    ElevatedButton.icon(
-                      icon: const Icon(Icons.qr_code),
-                      label: const Text("LER QR CODE"),
-                      style: ElevatedButton.styleFrom(
-                        minimumSize: const Size(200, 50),
-                      ),
-                      onPressed: () async {
-                        await Navigator.of(context).push(MaterialPageRoute(
-                          builder: (context) => const QRViewPage(),
-                        ));
-                        // initialize();
-                      },
-                    ),
-
-                    const SizedBox(height: 10),
-
-                    // 4. Botão VER TUTORIAL
-                    OutlinedButton.icon(
-                      icon: const Icon(Icons.help_outline),
-                      label: const Text("VER TUTORIAL"),
-                      style: OutlinedButton.styleFrom(
-                        minimumSize: const Size(200, 50),
-                      ),
-                      onPressed: () {
-                        Navigator.of(context).push(MaterialPageRoute(
-                          builder: (context) =>
-                              TutorialPage(), // importa TutorialPage
-                        ));
-                      },
-                    ),
-                    const SizedBox(height: 10),
-                    // VERIFICAR PERMISSÕES
-                    ElevatedButton.icon(
-                      icon: const Icon(Icons.verified_user),
-                      label: const Text("VERIFICAR PERMISSÕES"),
-                      style: ElevatedButton.styleFrom(
-                        minimumSize: const Size(200, 50),
-                      ),
-                      onPressed: () async {
-                        askPermissionBottomSheet(NavigationService.navigatorKey.currentContext);
-                      },
-                    ),
-                    const SizedBox(height: 10),
-                    // 5. Botão Política de Privacidade
-                    ElevatedButton.icon(
-                      icon: const Icon(Icons.privacy_tip),
-                      label: const Text("Política de Privacidade"),
-                      style: ElevatedButton.styleFrom(
-                        minimumSize: const Size(200, 50),
-                      ),
-                      onPressed: () {
-                        Navigator.of(context).push(MaterialPageRoute(
-                          builder: (context) => const PoliticaDePrivacidade(),
-                        ));
-                      },
-                    ),
-                  ],
-                ),
+                  // 2. Texto explicativo
+                  const Text(
+                    "Solicite o QR Code ao seu guardião para escanear e utilizar o app, mas antes clique em \"VER TUTORIAL\"",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ],
               ),
-            ),
+
+              const SizedBox(height: 10),
+
+              // 3. Botão LER QR CODE
+              ElevatedButton.icon(
+                icon: const Icon(Icons.qr_code),
+                label: const Text("LER QR CODE"),
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(200, 50),
+                ),
+                onPressed: () async {
+                  await Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => const QRViewPage(),
+                  ));
+                  // initialize();
+                },
+              ),
+
+              const SizedBox(height: 10),
+
+              // 4. Botão VER TUTORIAL
+              OutlinedButton.icon(
+                icon: const Icon(Icons.help_outline),
+                label: const Text("VER TUTORIAL"),
+                style: OutlinedButton.styleFrom(
+                  minimumSize: const Size(200, 50),
+                ),
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => TutorialPage(), // importa TutorialPage
+                  ));
+                },
+              ),
+              const SizedBox(height: 10),
+              // VERIFICAR PERMISSÕES
+              ElevatedButton.icon(
+                icon: const Icon(Icons.verified_user),
+                label: const Text("VERIFICAR PERMISSÕES"),
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(200, 50),
+                ),
+                onPressed: () async {
+                  askPermissionBottomSheet(NavigationService.navigatorKey.currentContext);
+                },
+              ),
+              const SizedBox(height: 10),
+              // 5. Botão Política de Privacidade
+              ElevatedButton.icon(
+                icon: const Icon(Icons.privacy_tip),
+                label: const Text("Política de Privacidade"),
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(200, 50),
+                ),
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => const PoliticaDePrivacidade(),
+                  ));
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
